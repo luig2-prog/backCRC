c0b7c49b22c86406369bf42c972728a0ca9b9aa9
luis hernandez <luisjimenezh8@gmail.com>
2021-11-07 12:52:40 -0500

    pasar

--- src/main/java/com/back/crc/controllers/mainController.java
+++ src/main/java/com/back/crc/controllers/mainController.java
@@ -1,10 +1,33 @@
 package com.back.crc.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.back.crc.utils.prueba;
+
+
+
 @RestController
-@RequestMapping("/api")
+@RequestMapping()
 public class mainController {
-	
+
+	@GetMapping("/prueba")
+//	@ResponseBody
+	public String getTime(){
+//	    time.capturedTime();
+		
+    return "prueba";
+	}
+//	
+//	@PostMapping("/prueba")
+//	@ResponseBody
+//	public prueba getTime(@RequestBody prueba str1){
+////	    time.capturedTime();
+//		
+//    return str1;
+//	}
 }
